93874baeb25684c2ca4a1d957f5747b18d671632
Kelvin M Matheka <beingmatheka@gmail.com>
2022-10-08 11:41:44 +0300

    working on side panel components

---
+++ music-player/src/Components/Sidebar/Like.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaMusic} from "react-icons/fa";
+
+const Like = () => {
+  return (
+    <div>
+      <h2>Welcome to M-Player <FaMusic /></h2>
+      <p>Hello user, this website is part of a simple react project developed by Kelvin Matheka using React Js. Feel free to clone it and add some features if you want to. Cheers 😝!</p>
+    </div>
+  )
+}
+
+export default Like
