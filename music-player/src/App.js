6b2029bf995ef2ecb8be08935024f8bda5090217
Kelvin M Matheka <beingmatheka@gmail.com>
2022-10-08 11:39:07 +0300

    rendering music player

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,11 +1,52 @@
 
+import React, {useState, useEffect} from 'react';
+import MusicPlayer from './Components/MusicPlayer';
+import Liblary from './Components/Sidebar/Liblary'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SideBar from './Components/SideBar'
+import Like from './Components/Sidebar/Like'
+
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  const[songs, setSongs] = useState(null)
 
+    const[currentSongI, setCurrentSongI] = useState(0)
+    const[nextSongI, setNextSongI] = useState(currentSongI + 1)
+  
+    useEffect(() =>{
+
+      fetch('http://localhost:8000/data')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        console.log(data)
+        setSongs(data)  
+      })
+
+    },[])
+    
+
+  return (
+    <Router>
+   <>
+   <div>
+    <header>
+    M- Player 
+    </header>
+   </div>
+   <SideBar />
+    <div className="App">
+      
+    
+    </Switch>
     </div>
+    </>
+    </Router>
+ 
+    
   );
 }
 
