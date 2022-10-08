a31e536174e0e0c5f6c66ff1a1b2e0765fb95f5b
Kelvin M Matheka <beingmatheka@gmail.com>
2022-10-08 11:49:27 +0300

    Working on the siidebar component

---
+++ music-player/src/Components/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {  FaHome, FaHeart, FaGithub } from "react-icons/fa";
+import { Link } from 'react-router-dom'
+
+function SideBar() {
+  return (
+    <div className="side">
+      <Link to= '/developer' >Developer <FaGithub /></Link>
+      <Link to= '/home'>Home <FaHome/></Link>
+      <Link to= '/like'>About my page <FaHeart/></Link>
+     </div>
+  )
+}
+
+export default SideBar
